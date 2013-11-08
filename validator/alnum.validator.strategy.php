28a07051e282db69642a2b36fb58f6e32d4d1e70
slier <slier81@gmail.com>
2013-11-08 18:05:17 +0800

    add validator/alnum.validator.strategy.php

---
+++ validator/alnum.validator.strategy.php
@@ -0,0 +1,55 @@
+<?php
+
+abstract class AlnumValidatorStrategy extends ValidatorStrategy
+{
+
+    /**
+     * @param $length
+     */
+    protected function configValidatorLengthAttr( $length )
+    {
+        if( is_null( $length ) ){
+            $this->data['min_length'] = 0;
+            $this->data['max_length'] = 0;
+        }
+        else{
+            if( is_array( $length ) ){
+                if( $this->is_assoc( $length ) ){
+                    if( count( $length ) > 1 ){
+                        $this->data['min_length'] = $length['min'];
+                        $this->data['max_length'] = $length['max'];
+                    }
+                    else{
+                        $this->data['min_length'] = isset( $length['min'] ) ? $length['min'] : 1;
+                        $this->data['max_length'] = isset( $length['max'] ) ? $length['max'] : 0;
+                    }
+                }
+                else{
+                    if( count( $length ) > 1 ){
+                        $this->data['min_length'] = $length[0];
+                        $this->data['max_length'] = $length[1];
+                    }
+                    else{
+                        $this->data['min_length'] = $length['min'];
+                        $this->data['max_length'] = 0;
+                    }
+                }
+            }
+            else{
+                $this->data['min_length'] = $length;
+                $this->data['max_length'] = 0;
+            }
+        }
+
+    }
+
+
+    /**
+     * @param $array
+     * @return bool
+     */
+    protected function is_assoc( $array )
+    {
+        return (bool)count( array_filter( array_keys( $array ), 'is_string' ) );
+    }
+}
