5a04ef7c3834c4ec52858b6ae00ad5c05ba5cf3d
slier <slier81@gmail.com>
2013-11-11 03:25:42 +0800

    refactor exception.php and change display style

--- exception.handler.php
+++ exception.handler.php
@@ -29,17 +29,18 @@ class ExceptionHandler
      */
     public function exceptionHandler( \Exception $e )
     {
-        echo '<div style="background-color:#eee;border:1px dashed #999999;padding:10px;margin-top:60px;margin-left:auto;margin-right:auto;width:1000px">' . '<br>';
-        echo '<h3>Exception Has Occured</h3>';
-        echo "<p>Code: {$e->getCode()} </p>";
-        echo "<p>Line: {$e->getLine()} </p>";
-        echo "<p>File: {$e->getFile()} </p>";
-        echo "<p>Message: {$e->getMessage()}</p>";
-        echo '<p>Stack Trace:</p>';
-        echo "<pre>";
-        print_r( $e->getTraceAsString() );
-        echo "</pre>";
-        echo '</div>';
+        $styles = 'width:40%;height:25%;background-color:#eee;
+                    border:1px dashed #999;position:absolute;
+                    top:-100;bottom:0;left:0;right:0;margin:auto;
+                    padding:20px;z-index:99';
+
+        $template = '<div style="%s">%s%s%s%s</div>';
+
+        printf( $template, $styles, '<h2>Exception Has Occured</h2>',
+            sprintf( '<p>Line:%s</p>', $e->getLine() ),
+            sprintf( '<p>File:%s</p>', $e->getFile() ),
+            sprintf( '<p>Message:%s</p>', $e->getMessage() )
+        );
     }
 
 
